76a961bc359053ac9fe28500c9e105bbeacd2286
yuhongxi@flashhold.com <yuhongxi@flashhold.com>
2018-12-18 17:40:38 +0800

    练习1.8的四大金刚

---
+++ src/main/java/com/demo/jdk8/Application.java
@@ -0,0 +1,20 @@
+package com.demo.jdk8;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+/**
+ * @Description:
+ * @author: yuhongxi
+ * @date:2018/12/18
+ */
+public class Application {
+    public static void main(String[] args) {
+        Consumer<Person> consumer= person -> person=new Person();
+        Function<Person,User> function= person -> new User();
+        Predicate<Person> p= person -> false;
+        Supplier<Person> s= Person::new;
+    }
+}
---
+++ src/main/java/com/demo/jdk8/User.java
@@ -0,0 +1,45 @@
+package com.demo.jdk8;
+
+/**
+ * @Description:
+ * @author: yuhongxi
+ * @date:2018/12/18
+ */
+public class User {
+    private String name;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
